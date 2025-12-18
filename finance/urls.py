1f350742bc3cbc4466887b5f51acbcdf890057fc
Olayinka Alawode <alawodeolayinka2@gmail.com>
2025-12-18 23:55:48 +0100

    new commit

--- finance/urls.py
+++ finance/urls.py
@@ -4,6 +4,7 @@ from . import views
 from .views import dashboard_api
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),         # /api/
-    path('dashboard/', dashboard_api, name='dashboard-api'),  # /api/dashboard/
+    path('', views.dashboard, name='dashboard'),
+    path("api/dashboard/", dashboard_api, name="dashboard-api"),
 ]
+
