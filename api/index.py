2391ad80f6fb5582297bd1a72d4d7bfd55bd1aee
Olayinka Alawode <alawodeolayinka2@gmail.com>
2025-12-18 23:39:45 +0100

    new commit

--- api/index.py
+++ api/index.py
@@ -1,2 +1,13 @@
-from greenhouse.wsgi import application
+import os
+import sys
+
+# Add project root to path
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+# Set settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greenhouse.settings')
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
 app = application
