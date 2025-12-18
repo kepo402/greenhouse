480a456f385bcd482cbdebdcb5eb02cd51108604
Olayinka Alawode <alawodeolayinka2@gmail.com>
2025-12-19 00:55:07 +0100

    new commit

--- greenhouse/settings.py
+++ greenhouse/settings.py
@@ -78,11 +78,19 @@ WSGI_APPLICATION = 'greenhouse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'yhZoHBQkhYKzTJGEEFtPewVvDAWmuYzI',
+        'HOST': 'gondola.proxy.rlwy.net',
+        'PORT': '55994',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
+
 #database railway: supportive unity
 
 
